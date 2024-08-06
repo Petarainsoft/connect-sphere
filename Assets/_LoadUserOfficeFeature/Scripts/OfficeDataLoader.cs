c4b7e7d223013e9c81552275ebc1d0486e35e19a
MinhPhan_win\TrietHuynh <triethuynh4403@gmail.com>
2024-08-06 10:16:00 +0700

    fix: duplicate creating office data objects

--- Assets/_LoadUserOfficeFeature/Scripts/OfficeDataLoader.cs
+++ Assets/_LoadUserOfficeFeature/Scripts/OfficeDataLoader.cs
@@ -7,22 +7,41 @@ namespace ConnectSphere
 {
     public class OfficeDataLoader : MonoBehaviour
     {
-        [SerializeField] OfficeSO _officeSO;
         [SerializeField]
-        private GameObject _itemPrefabs;
-        private GridLayoutGroup layout;
+        OfficeSO _officeSO;
 
-        private List<GameObject> items = new List<GameObject>();
+        [SerializeField]
+        private GameObject _officeItemPrefabs;
+
+        [SerializeField]
+        private GameObject _searchBar;
+        private GridLayoutGroup _layoutHolderOfficeData;
+        private List<GameObject> _offices = new List<GameObject>();
+        private TMP_InputField _searchField;
+        private const int NUMBER_COLUMNS_IN_ROW = 3;
+        private const int SECONDS_IN_MINUTE = 60;
+        private const int SECONDS_IN_HOUR = 3600;
+        private const int SECONDS_IN_DAY = 3600 * 24;
 
         // Start is called before the first frame update
         void Start()
         {
-            layout = GetComponent<GridLayoutGroup>();
+            _layoutHolderOfficeData = GetComponent<GridLayoutGroup>();
             RectTransform rect = GetComponent<RectTransform>();
             rect.sizeDelta = new Vector2(rect.sizeDelta.x, CalculateHeight(_officeSO.Count));
+            _searchField = _searchBar.GetComponentInChildren<TMP_InputField>();
+            _searchField.interactable = false;
 
             FillData();
             Debug.Log(transform.childCount);
+        }
+
+        public void Search()
+        {
+            for (int i = 0; i < _officeSO.Count; i++)
+            {
+                _offices[i].SetActive(_officeSO[i].name.Contains(_searchField.text));
+            }
         }
 
         private void FillData()
@@ -30,55 +49,49 @@ namespace ConnectSphere
             int count = _officeSO.Count;
             for (int i = 0; i < count; i++)
             {
+                GameObject office = Instantiate(_officeItemPrefabs, transform);
+                Debug.Log(office.name);
 
-                GameObject item = Instantiate(_itemPrefabs, transform);
-                Debug.Log(item.name);
-
-                FillItemData(item, i);
-                items.Add(item);
-
+                FillItemData(office, i);
+                _offices.Add(office);
             }
+            _searchField.interactable = true;
         }
 
-        private void FillItemData(GameObject item, int i)
+        private void FillItemData(GameObject office, int i)
         {
-            Debug.Log(item.transform.GetComponentInChildren<TextMeshProUGUI>().text);
-            item.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _officeSO[i].name;
+            Debug.Log(office.transform.GetComponentInChildren<TextMeshProUGUI>().text);
+            office.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = _officeSO[i].name;
             int seconds = _officeSO[i].GetDateDiff();
-            if (_officeSO[i].GetDateDiff() < 60)
+            if (_officeSO[i].GetDateDiff() < SECONDS_IN_MINUTE)
             {
-                item.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Last minute";
-
+                office.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Last minute";
             }
-            else if (seconds < 3600)
+            else if (seconds < SECONDS_IN_HOUR)
             {
-                item.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = seconds % 60 + "-minute ago";
-
+                office.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =
+                    seconds % SECONDS_IN_MINUTE + "-minute ago";
             }
-
-            else if (seconds < 3600 * 24)
+            else if (seconds < SECONDS_IN_DAY)
             {
-                item.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = seconds % 3600 + "-hour ago";
+                office.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =
+                    seconds % SECONDS_IN_HOUR + "-hour ago";
             }
             else
             {
-                item.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = seconds % (3600 * 24) + "-day ago";
-
+                office.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text =
+                    seconds % SECONDS_IN_DAY + "-day ago";
             }
         }
 
         private float CalculateHeight(int count)
         {
-            float elementHeight = layout.cellSize.y;
-            float spacingTop = layout.spacing.y;
+            float elementHeight = _layoutHolderOfficeData.cellSize.y;
+            float spacingTop = _layoutHolderOfficeData.spacing.y;
 
-            return elementHeight * (count / 3) + spacingTop * count - spacingTop;
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            return elementHeight * (count / NUMBER_COLUMNS_IN_ROW)
+                + spacingTop * count
+                - spacingTop;
         }
     }
 }
